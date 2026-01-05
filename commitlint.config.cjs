2032f48f63674c5683918605b29c9971815b90a8
callu9 <callu_9ine@naver.com>
2026-01-05 15:38:12 +0900

    chore: initialize project

---
+++ commitlint.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "ci",
+      ],
+    ],
+    "subject-case": [0],
+    "header-max-length": [2, "always", 100],
+  },
+};
